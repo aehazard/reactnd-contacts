de0e4f726087c06f081a98842ad7d29763302145
aehazard <32916110+aehazard@users.noreply.github.com>
2021-03-01 10:06:54 -0600

    CreateContact added

---
+++ src/CreateContact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CreateContact extends Component {
+  render() {
+    return(
+      <div>
+        Create New Contact!
+      </div>
+    )
+  }
+}
+
+export default CreateContact
